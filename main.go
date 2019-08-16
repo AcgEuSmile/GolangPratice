581d474768b63f2b9484f7bb5ee9f13400fc4975
acgeusmile <acgeusmile@gmail.com>
2019-08-16 17:36:03 +0800

    Using two type of declare variable

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func variable()	string{
+	// Decalre variable testing
+	// Method 1, :=
 	msg := "Drone CI/CD testing!!"
+	// Method 2, var msg string
+	var msg string
+	msg = "Drone  CI/CD testing~~"
 	return msg
 }
 
 func main() {
-
 	fmt.Println(variable())
 	fmt.Println("Running main.go successfully")
 }
